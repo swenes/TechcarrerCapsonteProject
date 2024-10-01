6c20dd5f0b975e736ef83f5fb7d79c91ca308409
Enes Aydoğdu <enes_aydogdu2001@hotmail.com>
2024-10-01 21:10:47 +0300

    added empty

--- src/main/java/Base/TestData.java
+++ src/main/java/Base/TestData.java
@@ -2,14 +2,19 @@ package Base;
 
 public class TestData {
 
+
     public String URL = "https://www.beymen.com/tr";
     public String name = "Enes";
     public String surname = "Aydogdu";
     public String phoneNumber = "5436234972";
     public String mail = "1aydogduenes@gmail.com";
     public String password = "parola.şifre1";
-    public String wrongMail = "abcabc@gmail.com";
+    public String invalidMail = "abcabc@sdfsd.com";
     public String wrongPassword = "123456";
+    public String empty = "";
+
+    //expected error messages
+    public String emptyError = "Bu alan boş bırakılamaz";
 
 
 }
