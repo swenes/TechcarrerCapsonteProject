6a7f9c68c321ea5d13ad4e6783c425c18d05bfde
Enes Aydoğdu <enes_aydogdu2001@hotmail.com>
2024-10-01 16:42:04 +0300

    edited test data

--- src/main/java/Base/TestData.java
+++ src/main/java/Base/TestData.java
@@ -2,12 +2,14 @@ package Base;
 
 public class TestData {
 
-    public String URL = "https://www.beymen.com/tr/customer/login?returnUrl=/tr/customer";
+    public String URL = "https://www.beymen.com/tr";
     public String name = "Enes";
     public String surname = "Aydogdu";
     public String phoneNumber = "5436234972";
     public String mail = "1aydogduenes@gmail.com";
-    public String password = "1234";
+    public String password = "parola.şifre1";
+    public String wrongMail = "abcabc@gmail.com";
+    public String wrongPassword = "123456";
 
 
 }
